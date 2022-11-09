db10ed33cee49bcb6b1adb5001a144f09e191add
vulrikkeholm <115697381+vulrikkeholm@users.noreply.github.com>
2022-11-09 15:35:29 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const saveEl = document.getElementById("save-el")
+const countEl = document.getElementById("count-el")
+let count = 0
+let topText = document.getElementById("top-text")
+const container = document.getElementById("container")
+const incrementBtn = document.getElementById('increment-btn')
+const saveBtn = document.getElementById('save-btn')
+
+incrementBtn.addEventListener('click', increment())
+saveBtn.addEventListener('click', save())
+
+console.log('det virker')
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+    container.style.backgroundImage = "url(Beethoven.jpg)"
+    topText.textContent = "Let me help!"
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0 
+    count = 0 
+    container.style.backgroundImage = "url(Smiling.jpg)"
+    topText.textContent = "We did it!"
+}
